3bfff671c426e655e1ea9d257162c428baf02bff
Christian Galvez <christian.javalera@teamknowlogy.com>
2024-05-02 16:04:07 -0700

    FBDEV-18 Title changed

--- apps/frontend/src/app/features/form-builder/home-builder.component.ts
+++ apps/frontend/src/app/features/form-builder/home-builder.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'builder-home',
     template: `
-    <h2>Hello Home Builder!</h2>
+    <h2>Hello Home Builder!!!!</h2>
     `
 })
 export class HomeBuilderComponent {}
